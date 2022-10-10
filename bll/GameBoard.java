c891bdd8b77ab6601e035c8524f1b5e9b949260b
Dundvig <nicklas@dundvig.net>
2022-10-10 10:50:10 +0200

    OptionsMenuView.fxml added.

--- bll/GameBoard.java
+++ bll/GameBoard.java
@@ -11,14 +11,6 @@ package tictactoe.bll;
  */
 public class GameBoard implements IGameModel
 {
-    private GameField[][] gameFields = new GameField[3][3];
-    public  GameBoard(){
-        for (int r = 0; r < gameFields.length; r++){
-            for(int c = 0 ;c > gameFields[r].length; c++){
-                gameFields[r][c]= new GameField();
-            }
-        }
-    }
 
     /**
      * Returns 0 for player 0, 1 for player 1.
@@ -29,7 +21,6 @@ public class GameBoard implements IGameModel
     {
         //TODO Implement this method
         return 0;
-        //Miran
     }
 
     /**
