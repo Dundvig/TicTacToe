7ca5ad2725ea66fa0d8f64f19969f38afc163af3
Dundvig <nicklas@dundvig.net>
2022-10-10 12:22:04 +0200

    Dundvig

---
+++ gui/controller/OptionsMenuController.java
@@ -0,0 +1,18 @@
+package tictactoe.gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+
+public class OptionsMenuController {
+
+    public Button btnMainMenu;
+    public Button btnRayTrace;
+    public ComboBox cmbSkins;
+    public ComboBox cmbMusicBackground;
+    public Slider sldMusicVolume;
+    public Slider sldSoundFxVolume;
+    public ImageView imgOptMenu;
+}
